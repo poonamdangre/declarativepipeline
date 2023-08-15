ba6f34b8ffb69890e9827f1dad574d211d8fdc7e
punamkosare <pkosare1997@gmail.com>
2023-08-15 15:55:36 +0530

    pull

---
+++ declarativepipeline.groovy
@@ -0,0 +1,37 @@
+pipeline{
+    agent any
+    stages{
+        stage('PULL'){
+            steps{
+                git branch: 'master', url: 'https://github.com/poonamdangre/atulyw-java-tomcat-maven-example.git'
+           echo 'pull completed'
+            }
+        }
+        /*
+        stage('build'){
+            steps{
+                
+                sh'/opt/apache-maven-3.9.4/bin/mvn  package'
+             echo'build completed'
+            }
+        }
+        stage('test completed'){
+         steps{   
+            withSonarQubeEnv(installationName: 'Sonar-server', credentialsId: 'sonartoken1') {
+    
+    
+sh'/opt/apache-maven-3.9.4/bin/mvn sonar:sonar \
+  -Dsonar.projectKey=mypro'
+
+            echo 'test completed'
+            }   
+        }
+        }
+        stage('deploy'){
+            steps{
+                echo 'deploy complete'
+            }
+        }
+    }
+}
+*/
