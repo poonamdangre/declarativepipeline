80b8fed625ca17226436437ec95a569b13d1e4b7
punamkosare <pkosare1997@gmail.com>
2023-08-15 17:06:47 +0530

    test1

--- declarativepipeline.groovy
+++ declarativepipeline.groovy
@@ -17,19 +17,20 @@ pipeline{
         }
     }
 }
-        /*
+        
         stage('test completed'){
          steps{   
-            withSonarQubeEnv(installationName: 'Sonar-server', credentialsId: 'sonartoken1') {
+        
     
     
-sh'/opt/apache-maven-3.9.4/bin/mvn sonar:sonar \
-  -Dsonar.projectKey=mypro'
+mvn sonar:sonar   -Dsonar.projectKey=freshproject   -Dsonar.host.url=http://3.88.116.75:9000   -Dsonar.login=dd7078997393eb564843e5e2089d2c403f3a6e16
+
 
             echo 'test completed'
-            }   
+             
         }
         }
+        /*
         stage('deploy'){
             steps{
                 echo 'deploy complete'
