0370e143867d417e6d1dc3c60e31818ae7085a5d
punamkosare <pkosare1997@gmail.com>
2023-08-15 17:21:15 +0530

    test4

--- declarativepipeline.groovy
+++ declarativepipeline.groovy
@@ -23,7 +23,7 @@ pipeline{
         
     
     
-/opt/apache-maven3.9.4/bin/mvn sonar:sonar   -Dsonar.projectKey=freshproject   -Dsonar.host.url=http://3.88.116.75:9000   -Dsonar.login=dd7078997393eb564843e5e2089d2c403f3a6e16
+/opt/apache-maven3.9.4/bin/mvn sonar:sonar   -Dsonar.projectKey=freshproject      
 
 
             echo 'test completed'
