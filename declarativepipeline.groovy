c33d6e4fb5f105d4a78deaaeceda4cbe6a13989a
punamkosare <pkosare1997@gmail.com>
2023-08-19 18:09:25 +0530

    test20

--- declarativepipeline.groovy
+++ declarativepipeline.groovy
@@ -23,7 +23,7 @@ pipeline{
             withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar-token') {
      
     sh'/opt/apache-maven-3.9.4/bin/mvn sonar:sonar \
-  -Dsonar.projectKey=javaproject'
+  -Dsonar.projectKey=mavenproject'
   echo 'test completed'
             }           
         }
